c0b0bb2e3a01e48de709ea022e11baeda4977750
choiyounho <maxosa78@gmail.com>
2020-09-01 12:46:51 +0900

    [feat] : ustils 패키징, 하드코딩 상수화 CommonsCoonstant 클래스 구현

---
+++ src/main/java/utils/CommonsConstant.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class CommonsConstant {
+
+    private CommonsConstant(){}
+
+    public static final String NAVER_MAP_API_KEY_ID = "X-NCP-APIGW-API-KEY-ID";
+    public static final String NAVER_MAP_API_KEY_SECRET = "X-NCP-APIGW-API-KEY";
+    public static final String NAVER_MAP_CLIENT_ID = "vsp3juk0mt";
+    public static final String NAVER_MAP_CLIENT_SECRET = "FKsrUmwNCoSZDTge37d5OUkNN3Sq3MYMYXDSrCny";
+
+    public static final String KEY_ADDRESSES = "addresses";
+    public static final String UTF_8 = "UTF-8";
+    public static final String HTTP_METHOD_GET = "GET";
+    public static final int HTTP_STATE_CODE_200_OK = 200;
+
+}
